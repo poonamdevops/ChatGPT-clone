1de71edce40ddabd6344cc63f37549318932b510
sandeep <wd.sandeepnayak@gmail.com>
2023-09-23 00:08:27 +0530

    done massive changes

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,10 @@
 FROM python:3.10
 
-RUN apt-get update && apt-get install -y \
-    nginx
-
 WORKDIR /aiBot
 
 COPY . /aiBot/
 
-RUN pip install -r requirements.txt
+# RUN pip install -r requirements.txt
 
 RUN chmod +x envSetup.sh
 
@@ -17,4 +14,4 @@ EXPOSE 8000
 
 ENV DJANGO_SETTINGS_MODULE=django_chatbot.settings
 
-CMD service nginx start && python3 /aiBot/manage.py runserver 0.0.0.0:8000
+CMD python3 /aiBot/manage.py runserver 0.0.0.0:8000
--- envSetup.sh
+++ envSetup.sh
@@ -1,2 +1,43 @@
 #!/bin/bash
-mkdir /itsWorking
+apt-get update
+
+apt-get install python3-pip python3-dev nginx
+
+pip3 install virtualenv
+
+cd /aiBot
+
+virtualenv env
+
+source env/bin/activate
+
+pip install -r requirements.txt
+
+pip install django gunicorn
+
+django-admin startproject textutils /aiBot
+
+python3 /aiBot/manage.py makemigrations
+python3 /aiBot/manage.py migrate
+
+ufw allow 8000
+
+gunicorn --bind 0.0.0.0:8000 textutils.wsgi
+
+deactivate
+
+cat /aiBot/unicornSocket.txt > /etc/systemd/system/gunicorn.socket
+
+cat /aiBot/unicornService.txt > /etc/systemd/system/gunicorn.service
+
+systemctl start gunicorn.socket
+
+systemctl enable gunicorn.socket
+
+cat /aiBot/RPutils.txt > /etc/nginx/sites-available/textutils
+
+ln -s /etc/nginx/sites-available/textutils /etc/nginx/sites-enabled/
+
+systemctl restart nginx
+
+
